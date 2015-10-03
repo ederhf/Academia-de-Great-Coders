28d28c37265310a323df1240dbd494845b47f0ed
Éder <ederhf@ciant.com>
2015-10-03 10:27:17 -0300

    Introduction to C#

---
+++ C# Fundamentals with C# 5.0/Introduction to C#/Hello/Hello/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu Nome:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Quantas horas você dormiu na última noite?");
+            int horasDormidas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + name);
+
+            if (horasDormidas < 8)
+            {
+                Console.WriteLine("Você deve estar cansado!");
+            }
+            else
+            {
+                Console.WriteLine("Você parece estar bem descansado!");
+            }
+        }
+    }
+}
+
