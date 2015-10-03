bd06d5e05295e8f7e4f349bdbd474097fb0fc383
Éder <ederhf@ciant.com>
2015-10-03 15:29:01 -0300

    Classes and Objects in C#

---
+++ C# Fundamentals with C# 5.0/Classes and Objects in C#/Grades/Grades/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(77);
+            book.AddGrade(54.25f);
+            book.AddGrade(41f);
+            
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.LowestGrade);
+            Console.WriteLine(stats.HighestGrade);
+
+        }
+    }
+}
