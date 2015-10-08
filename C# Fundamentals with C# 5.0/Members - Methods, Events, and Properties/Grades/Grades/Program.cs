19bb81543cf94a9977dbed20a8867fd85df50433
Éder <ederhf@ciant.com>
2015-10-07 21:55:55 -0300

    C# Fundamentals with C# 5.0: Members - Methods, Events, and Properties

---
+++ C# Fundamentals with C# 5.0/Members - Methods, Events, and Properties/Grades/Grades/Program.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook("Éder's book");
+            book.AddGrade(77);
+            book.AddGrade(54.25f);
+            book.AddGrade(41f);
+            
+            GradeStatistics stats = book.ComputeStatistics();
+            
+            book.NameChanged += OnNameChanged;
+            book.NameChanged += OnNameChanged;
+            book.NameChanged += OnNameChanged2;
+            book.NameChanged -= OnNameChanged;
+
+            book.Name = "Henrique's book";
+
+            WriteNames(book.Name);
+            
+
+            //Console.WriteLine(stats.AverageGrade);
+            //Console.WriteLine(stats.LowestGrade);
+            //Console.WriteLine(stats.HighestGrade);
+
+        }
+
+        private static void OnNameChanged2(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine("*****");
+        }
+
+        private static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine("Name changed from {0} to {1}", args.OldValue, args.NewValue);
+        }
+
+        private static void WriteBytes(int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+
+            WriteByteArray(bytes);
+        }
+
+        private static void WriteBytes(float value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+
+            WriteByteArray(bytes);
+        }
+
+        private static void WriteByteArray(byte[] bytes)
+        {
+            foreach (byte b in bytes)
+            {
+                Console.Write("0x{0:X} {2} {1} ", b, "Éder", "Franco");
+            }
+            Console.WriteLine();
+        }
+
+        private static void WriteNames(params string[] names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
