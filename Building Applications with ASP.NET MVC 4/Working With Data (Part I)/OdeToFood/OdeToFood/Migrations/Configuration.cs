8d8b92862835ac6db1c9553207c134a0befa3605
Éder <ederhf@ciandt.com>
2015-10-26 22:30:03 -0200

    Building Applications with ASP.NET MVC 4: Working With Data (Part I)

---
+++ Building Applications with ASP.NET MVC 4/Working With Data (Part I)/OdeToFood/OdeToFood/Migrations/Configuration.cs
@@ -0,0 +1,35 @@
+namespace OdeToFood.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using OdeToFood.Models;
+    using System.Collections.Generic;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OdeToFood.Models.OdeToFoodDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(OdeToFood.Models.OdeToFoodDb context)
+        {
+            context.Restaurants
+                .AddOrUpdate(r => r.Name,
+                new Restaurant { Name = "Sabatino", City = "Baltimore", Country = "USA" },
+                new Restaurant { Name = "Gordão Lanches", City = "Valinhos", Country = "Brazil" },
+                new Restaurant
+                {
+                    Name = "Smaka",
+                    City = "Gothenburg",
+                    Country = "Sweden",
+                    Reviews =
+                        new List<RestaurantReview> {
+                            new RestaurantReview { Rating = 9, Body = "Great Food!", ReviewerName = "Éder" }
+                        }
+                });
+        }
+    }
+}
