aeb7187924f874e31667fdcd22bd047aeba5ed07
Éder <ederhf@ciandt.com>
2015-11-04 23:53:50 -0200

    Building Applications with ASP.NET MVC 4: Security and ASP.NET MVC 4

---
+++ Building Applications with ASP.NET MVC 4/Security and ASP.NET MVC 4/OdeToFood/OdeToFood/Models/RestaurantReview.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OdeToFood.Models
+{
+    public class RestaurantReview : IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [Range(1,10)]
+        [Required]
+        public int Rating { get; set; }
+
+        [Required]
+        [StringLength(1024)]
+        public string Body { get; set; }
+
+        [Display(Name="User Name")]
+        [DisplayFormat(NullDisplayText="Anonymous")]
+        public string ReviewerName { get; set; }
+        public int RestaurantId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Rating < 2 && ReviewerName.ToLower().StartsWith("eder"))
+            {
+                yield return new ValidationResult("Desculpe, Éder, Você não pode fazer isso");                
+            }
+        }
+    }
+}
